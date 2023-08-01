dd0a8ac89492ea94a5a6584bc5dac762518f505c
Sander Descamps <sander_descamps@hotmail.com>
2023-08-01 16:13:42 +0200

    fix: cleanup test commands

--- main.go
+++ main.go
@@ -61,95 +61,6 @@ var runCmd = &cobra.Command{
 	},
 }
 
-var testCmd = &cobra.Command{
-	Use:   "test",
-	Short: "Print the version number of Hugo",
-	Long:  `All software has versions. This is Hugo's`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("this is just a test")
-	},
-}
-
-var test2Cmd = &cobra.Command{
-	Use:   "test2",
-	Short: "Print the version number of Hugo",
-	Long:  `All software has versions. This is Hugo's`,
-	Run: func(cmd *cobra.Command, args []string) {
-
-		commvaultTarget, _ := cmd.Flags().GetString("endpoint")
-		username, _ := cmd.Flags().GetString("user")
-		password, _ := cmd.Flags().GetString("password")
-		insecure, _ := cmd.Flags().GetBool("insecure")
-
-		// var client client.CommvaultClient
-		client, err := client.NewClient(commvaultTarget, username, password, insecure)
-		if err != nil {
-			fmt.Fprintf(cmd.ErrOrStderr(), "%s\n", err)
-			os.Exit(1)
-		}
-
-		bclients, err := client.Bkp.GetBkpClients()
-		if err != nil {
-			fmt.Fprintf(cmd.ErrOrStderr(), "%s\n", err)
-			os.Exit(1)
-		}
-		for _, bc := range bclients {
-			fmt.Printf("CLIENT clientId=%d hostname: %s company=%s\n", bc.Client.ClientEntity.ClientID, bc.Client.ClientEntity.HostName, bc.Client.ClientEntity.EntityInfo.CompanyName)
-
-			bagents, err := client.Bkp.GetBkpAgents(uint64(bc.Client.ClientEntity.ClientID))
-			if err != nil {
-				fmt.Fprintf(cmd.ErrOrStderr(), "%s\n", err)
-				os.Exit(1)
-			}
-			for _, ba := range bagents {
-				fmt.Printf("AGENT clientId=%d clientName='%s' appName='%s' applicationId=%d\n", ba.IdaEntity.ClientID, ba.IdaEntity.ClientName, ba.IdaEntity.AppName, ba.IdaEntity.ApplicationID)
-			}
-
-			binstances, err := client.Bkp.GetBkpInstancesByClientId(uint64(bc.Client.ClientEntity.ClientID))
-			if err != nil {
-				fmt.Fprintf(cmd.ErrOrStderr(), "%s\n", err)
-				os.Exit(1)
-			}
-			for _, bi := range binstances {
-				fmt.Printf("INSTANCE appName='%s' applicationSize=%d applicationId=%d\n", bi.Instance.AppName, bi.ApplicationSize, bi.Instance.ApplicationID)
-			}
-
-		}
-
-	},
-}
-
-var test3Cmd = &cobra.Command{
-	Use:   "test3",
-	Short: "Print the version number of Hugo",
-	Long:  `All software has versions. This is Hugo's`,
-	Run: func(cmd *cobra.Command, args []string) {
-
-		commvaultTarget, _ := cmd.Flags().GetString("endpoint")
-		username, _ := cmd.Flags().GetString("user")
-		password, _ := cmd.Flags().GetString("password")
-		insecure, _ := cmd.Flags().GetBool("insecure")
-
-		// var client client.CommvaultClient
-		client, err := client.NewClient(commvaultTarget, username, password, insecure)
-		if err != nil {
-			fmt.Fprintf(cmd.ErrOrStderr(), "%s\n", err)
-			os.Exit(1)
-		}
-
-		fmt.Printf("------------------\n")
-		dbs2, err := client.Database.GetDatabaseInstances()
-		if err != nil {
-			fmt.Fprintf(cmd.ErrOrStderr(), "%s\n", err)
-			os.Exit(1)
-		}
-
-		for _, db := range dbs2 {
-			fmt.Printf("%s\n", db.Instance.ClientName)
-		}
-	},
-}
-
 func main() {
 	var rootCmd = &cobra.Command{Use: "app"}
 	rootCmd.PersistentFlags().Int16P("port", "l", 2112, "Port the metricserver will listen on")
@@ -163,7 +74,7 @@ func main() {
 	rootCmd.MarkFlagsMutuallyExclusive("password", "token")
 	// #nosec G104
 	rootCmd.MarkFlagRequired("endpoint")
-	rootCmd.AddCommand(runCmd, testCmd, test2Cmd, test3Cmd)
+	rootCmd.AddCommand(runCmd)
 	// #nosec G104
 	rootCmd.Execute()
 }
